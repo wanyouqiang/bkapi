ac96dfc200bf2f800f6cc4c29270b2be7b5a4077
孟海龙 <2210411072@qq.com>
2018-01-11 13:46:08 +0800

    异常文件

---
+++ app/Exceptions/ApiException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * API异常父类，所有的子异常继承这个类.
+ * User: Yikai Yu
+ * Date: 2017/9/27
+ * Time: 16:43
+ */
+
+namespace App\Exceptions;
+
+
+class ApiException extends \Exception
+{
+    // 主要还是为了区分错误
+}

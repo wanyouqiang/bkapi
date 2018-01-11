f18513703ea6b17c52095440f5475be33226be2c
孟海龙 <2210411072@qq.com>
2018-01-11 09:39:09 +0800

    七牛云包引入

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // 七牛云
+        Overtrue\LaravelFilesystem\Qiniu\QiniuStorageServiceProvider::class,
 
     ],
 
